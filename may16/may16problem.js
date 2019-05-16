c19e437a71ccfd02fa2157b83b3e42188cdf04c1
David Friday <davidsfriday13@gmail.com>
2019-05-16 15:25:04 -0500

    practice problem may16

---
+++ may16/may16problem.js
@@ -0,0 +1,18 @@
+// Write a function that can determine whether a string is a palindrome (reads same backwards and forwards)
+
+let testPalidrome = 'loopl'
+
+function isPalindrom(string) {
+    counter = 0
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[string.length - i -1]) {
+        }
+        else {
+            return false
+        }
+
+    }
+    return true
+}
+
+console.log(isPalindrom(testPalidrome))
